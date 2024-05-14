590e6883a019936a9c2cbd36f459f281a97923be
Valeriy Smirnov <valeriy.5mirnov@yandex.ru>
2024-05-14 23:58:51 +0700

    style fixes

--- image-downloader.py
+++ image-downloader.py
@@ -1,13 +1,13 @@
-import requests
 import os
-import shutil
-import re
 import pyperclip
+import re
+import requests
+import shutil
 import time
 
+
 input('Press Enter to paste the adress')
-s = pyperclip.paste()
-s = str(s)
+s = str(pyperclip.paste())
 r = requests.get(s)
 pattern = r'href=\"(.+?\.png|.+?\.jpg|.+?\.gif)\"'
 time1 = time.strftime('%H-%M-%S', time.localtime())
@@ -16,7 +16,7 @@ os.mkdir(path1)
 os.chdir(path1)
 images = re.findall(pattern, r.text)
 for image in images:
-	r1 = requests.get('https://2ch.hk/' + image, stream = True)
+	r1 = requests.get('https://2ch.hk/' + image, stream=True)
 	if r1.status_code == 200:
 		(dirname, filename) = os.path.split(image)
 		with open(filename, 'wb') as file:
